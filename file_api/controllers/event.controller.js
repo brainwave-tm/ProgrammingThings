1ade78e0f241113973134f118d94bf72a1c9fcb4
hdeo12345 <36956061+hdeo12345@users.noreply.github.com>
2021-05-01 11:19:17 +0100

    Add armed system status to status request

--- file_api/controllers/event.controller.js
+++ file_api/controllers/event.controller.js
@@ -1,4 +1,4 @@
-const {gen, api_responses} = require("../utilities/create_response");
+const { gen, api_responses } = require("../utilities/create_response");
 const eventModel = require("../models/event.model");
 const _ = require("lodash");
 
@@ -16,21 +16,22 @@ const _ = require("lodash");
 
 module.exports.get = (req, res) => {
     // Accepts pagination parameters //
-    if(!req.query.offset || !req.query.limit) return res.send(gen({code: api_responses.INVALID_QUERY, message: "You must supply pagination parameters (offset/limit) with your request."}));
-    eventModel.find({}, {__v: 0, _id: 0}).sort("-timestamp").then(results => {
+    if (!req.query.offset || !req.query.limit) return res.send(gen({ code: api_responses.INVALID_QUERY, message: "You must supply pagination parameters (offset/limit) with your request." }));
+    eventModel.find({}, { __v: 0, _id: 0 }).sort("-timestamp").then(results => {
         let results_paginated = _.take(_.drop(results, req.query.offset), req.query.limit);
         results_paginated.forEach(element => {
-            if(element.type === "DETECTED_FACE") element.image = (process.env.LOCAL === "true" ? "http://localhost:5000/uploaded/faces/" : "https://api.homesecurity.jakestringer.dev/uploaded/faces/") + element.image;
+            if (element.type === "DETECTED_FACE") element.image = (process.env.LOCAL === "true" ? "http://localhost:5000/uploaded/faces/" : "https://api.homesecurity.jakestringer.dev/uploaded/faces/") + element.image;
         });
-        return res.send(gen({payload: results_paginated}));
+        return res.send(gen({ payload: results_paginated }));
     })
 }
 
-module.exports.status = (req, res) => {
-    // Get latest PI_STATUS //
-    eventModel.find({type: "PI_ONLINE"}, {value: 1, _id: 0}).sort("-timestamp").limit(1).then(result => {
-        if(result) {
-            return res.send(gen({payload: result[0]}));
-        } else return res.send(gen({code: api_responses.INVALID_QUERY}));
-    })
+module.exports.status = async (req, res) => {
+    // Get latest PI_STATUS and ARM_SYSTEM //
+    var piStatus = await eventModel.find({ type: "PI_ONLINE" }, { value: 1, _id: 0 }).sort("-timestamp").limit(1);
+    var armedStatus = await eventModel.find({ type: "ARM_SYSTEM" }, { value: 1, _id: 0 }).sort("-timestamp").limit(1);
+
+    if (piStatus && armedStatus) {
+        return res.send(gen({ payload: { piStatus: piStatus[0], armedStatus: armedStatus[0] } }));
+    } else return res.send(gen({ code: api_responses.INVALID_QUERY }));
 }
